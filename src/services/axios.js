1585d2434de628812125c27cbcdaefefb1042d2e
David Raj <dr.s.davidraj@gmail.com>
2026-06-17 15:07:46 +0530

    added /api in base url

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mony.bazhilgroups.in/",
+  baseURL: "https://mony.bazhilgroups.in/api",
 });
 
 // Attach token automatically
